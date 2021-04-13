5cd2886b04275e7052d4012e78b222e2f4deecfb
Jordan <phoaxy@gmail.com>
2021-04-13 13:43:11 -0400

    Added light-mode to localStorage

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,6 +2,7 @@
 let burger = document.querySelector(".burger");
 let navLinks = document.querySelector(".nav-links");
 let theme = document.querySelector("#theme-mode");
+let lightMode = localStorage.getItem("lightMode");
 
 nav = () =>{
     burger.addEventListener("click", ()=>{
@@ -12,10 +13,18 @@ nav = () =>{
 nav();
 
 theme.addEventListener("click", ()=>{
+    lightMode = localStorage.getItem("lightMode");
     document.body.classList.toggle("light-mode");
     if(document.body.classList.contains("light-mode")){
-        theme.className = "fas fa-moon"
+        theme.className = "fas fa-moon";
+        localStorage.setItem("lightMode", "enabled");
     }else{
-        theme.className = "fas fa-sun"
+        theme.className = "fas fa-sun";
+        localStorage.setItem("lightMode", null);
     }
 });
+
+if(lightMode === "enabled"){
+    document.body.classList.toggle("light-mode");
+    theme.className = "fas fa-moon";
+}
