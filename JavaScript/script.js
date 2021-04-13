22932d03031dc2736b3385e0d981aa6e35c7bfc1
Jordan <phoaxy@gmail.com>
2021-04-13 13:07:44 -0400

    Added light-mode (BETA)
    - Added light-mode
    - Changed styles of project divs

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,7 @@
 //Jordan Schulte
 let burger = document.querySelector(".burger");
 let navLinks = document.querySelector(".nav-links");
+let theme = document.querySelector("#theme-mode");
 
 nav = () =>{
     burger.addEventListener("click", ()=>{
@@ -9,3 +10,12 @@ nav = () =>{
     });
 }
 nav();
+
+theme.addEventListener("click", ()=>{
+    document.body.classList.toggle("light-mode");
+    if(document.body.classList.contains("light-mode")){
+        theme.className = "fas fa-moon"
+    }else{
+        theme.className = "fas fa-sun"
+    }
+});
