6a3fa34bf0f1552605b24e37c060efef118d3dbe
Jordan <phoaxy@gmail.com>
2021-04-14 12:36:44 -0400

    Added more animations
    - Added more animations
    - Cleaned up code

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,29 +5,31 @@ let theme = document.querySelector("#theme-mode");
 let lightMode = localStorage.getItem("lightMode");
 let icon = document.querySelector("#icon");
 
-nav = () =>{
-    burger.addEventListener("click", ()=>{
+nav = () => {
+    burger.addEventListener("click", () => {
         navLinks.classList.toggle("nav-active");
         burger.classList.toggle("toggle");
     });
 }
 nav();
 
-theme.addEventListener("click", ()=>{
+theme.addEventListener("click", () => {
     lightMode = localStorage.getItem("lightMode");
     document.body.classList.toggle("light-mode");
-    if(document.body.classList.contains("light-mode")){
+    if (document.body.classList.contains("light-mode")) {
         theme.className = "fas fa-moon";
         icon.href = "Images/logo-white.jpg";
         localStorage.setItem("lightMode", "enabled");
-    }else{
+        theme.classList.toggle("rotateForward");
+    } else {
         theme.className = "fas fa-sun";
         icon.href = "Images/logo.jpg";
         localStorage.setItem("lightMode", null);
+        theme.classList.toggle("rotateBackward");
     }
 });
 
-if(lightMode === "enabled"){
+if (lightMode === "enabled") {
     document.body.classList.toggle("light-mode");
     icon.href = "Images/logo-white.jpg";
     theme.className = "fas fa-moon";
