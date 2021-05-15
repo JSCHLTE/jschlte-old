904e1919145613e40400467409d61550121b7fd3
Jordan <phoaxy@gmail.com>
2021-05-15 18:23:55 -0400

    v1.8
    - Redesigned navbar.
    - Adjusted notification position.
    - Fixed hyperlinks.

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -2,11 +2,14 @@ let burger = document.querySelector(".burger");
 let navLinks = document.querySelector(".nav-links");
 const sections = document.querySelectorAll("section");
 const navbar = document.querySelector("nav");
+const body = document.querySelector("body");
+const navItems = document.querySelectorAll(".nav-items");
 
 nav = () => {
     burger.addEventListener("click", () => {
         navLinks.classList.toggle("nav-active");
         burger.classList.toggle("toggle");
+        body.classList.toggle("fixed-position");
     });
 }
 nav();
@@ -18,3 +21,11 @@ window.addEventListener("scroll", () =>{
         navbar.classList.remove("active");
     }
 });
+
+navItems.forEach(navItem=> {
+    navItem.addEventListener("click", () =>{
+      navLinks.classList.remove("nav-active");
+      burger.classList.remove("toggle");
+      body.classList.remove("fixed-position");
+    });
+  });
