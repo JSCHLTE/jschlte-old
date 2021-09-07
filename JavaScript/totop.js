0ea06d72e43061074792de3715da342d18ad0f0a
Jordan <phoaxy@gmail.com>
2021-09-07 16:05:40 -0400

    alpha v1
    - Redesigned the entire website.

--- JavaScript/totop.js
+++
@@ -1,9 +0,0 @@
-let toTop = document.querySelector(".btt");
-
-window.addEventListener("scroll", () =>{
-    if(window.scrollY > 200){
-        toTop.classList.add("btt-active")
-    } else {
-        toTop.classList.remove("btt-active")
-    }
-});
